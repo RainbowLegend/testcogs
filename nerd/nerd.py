3c044fdd7bac21d23fc04129044e3f440edb9ed7
RainbowLegend <rainbowlegend3054@gmail.com>
2018-08-14 13:36:32 -0400

    Update nerd.py

--- nerd/nerd.py
+++ nerd/nerd.py
@@ -29,33 +29,9 @@ class Nerd:
         await self.bot.say("Hello World!")
         
     @commands.command(pass_context=True)
-    @commands.has_any_role("Senior Moderator", "Moderator", "Administrator", "Game Night Moderator")
-    async def togglehost(self, ctx):
-        author = ctx.message.author
-        localRoleAdmin = discord.Role(id='414196360321957888', server='288455332173316106')
-        localRoleSeniorMod = discord.Role(id='414189641890267157', server='288455332173316106')
-        localRoleMod = discord.Role(id='414196360321957888', server='288455332173316106')
-        administrator = discord.Role(id='288457272663867392', server='288455332173316106')
-        seniorModerator = discord.Role(id='288464565791096838', server='288455332173316106')
-        moderator = discord.Role(id='288682024515141634', server='288455332173316106')
-
-        # Checks to find which role in the hierarchy they need
-
-        if moderator in author.roles:  #  If they are a Moderator...
-            localRole = localRoleMod
-        elif seniorModerator in author.roles: #  If they are a Senior Mod...
-            localRole = localRoleSeniorMod
-        elif administrator in author.roles:
-            localRole = localRoleAdmin
-        else:
-            return
-
-        if localRole in author.roles:  #  If the hosting role is in the author
-            await self.bot.remove_roles(author, localRole)  # Remove the role
-            await self.bot.say('Successfully removed the Hosting role.')
-        else:  # If the hosting role not in the author
-            await self.bot.add_roles(author, localRole)  # Add the role
-            await self.bot.say('Successfully added the Hosting role.')
+    @commands.has_any_role()
+    async def commandTwo(self, ctx):
+        
         
 
 
